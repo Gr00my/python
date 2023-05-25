45ec2715658c65427e767dc7edfc4ff5f87e0c07
Timkador <abcsmir.ant@gmail.com>
2023-05-25 11:53:25 +0700

    25.05.2023

---
+++ lesson 33/main.py
@@ -0,0 +1,56 @@
+# ================== 1 задача ==================
+# slovar = {"key1": "value1",
+#           "key2": "value2",
+#           "key3": "value3",
+#           "key4": "value4",
+#           "key5": "value5", }
+# keys = []
+# values = []
+# for (key, value) in slovar.items():
+#     keys.append(key)
+#     values.append(value)
+# print(keys)
+# print(values)
+# print(slovar)
+
+#===============================================
+
+# slovar = {"key1": "value1",
+#           "key2": "value2",
+#           "key3": "value3",
+#           "key4": "value4",
+#           "key5": "value5"}
+#
+# print(list(slovar.keys()))
+# print(list(slovar.values()))
+
+# ================== 2 задача ==================
+
+#x = ["Никита", "Екатерина", "Арсалан", "Наталья", "Григорий", "Евгений", "Анастасия", "Андрей", "Евгения", "Герман", "Тимур", "Ярослава", "Есения", "Даниил", "Данил"]
+#name = input("Введите имя: ").capitalize()
+#print(x.count(name))
+
+# ==============================================
+
+# x = ["Никита", "Екатерина", "Арсалан", "Наталья", "Григорий", "Евгений", "Анастасия", "Андрей", "Евгения", "Герман", "Тимур", "Ярослава", "Есения", "Даниил", "Данил"]
+# name = input("Введите имя: ").capitalize()
+# skolko = 0
+# for i in x:
+#     if i == name:
+#         skolko += 1
+# print(skolko)
+
+# ================== 3 задача ==================
+
+# import math
+# chiselko = float(input("Введите число: "))
+# print(math.ceil(chiselko))
+
+# ================== 4 задача ==================
+
+# N = int(input("Введите длину строки: "))
+# for i in range(N):
+#     bykvi = str(input("Введите букву английского алфавита: ")).lower()
+#     stroka = []
+# engalfabet = "abcdefghijklmnopqrstuvwxyz"
+# print(stroka)
